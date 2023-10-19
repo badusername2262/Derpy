bcdc3d1c22568790c515fa4b26febdcaf83fd649
Badusername2262 <badusername2262@gmail.com>
2023-10-19 12:55:48 +1000

    ahhh

---
+++ Engine/src/Derpy/Events/Event.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace DERPY
+{
+
+    enum class EventType
+    {
+        None = 0,
+        WindowClose, WindowResised, WindowFocus, WindowLostFocuse, WindowMoved,
+        EngineTick, EngineUpdate, EngineRender,
+        KeyPressed, KeyReleased,
+        MouseButtonPressed, MouseButtonReleasted, MouseMoved, MouseScrolled
+    };
+
+}
