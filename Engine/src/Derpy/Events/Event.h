92564ef652de8fb9ac17726793fbec6577ca4833
Badusername2262 <badusername2262@gmail.com>
2023-10-20 14:38:32 +1000

    event.h update

--- Engine/src/Derpy/Events/Event.h
+++ Engine/src/Derpy/Events/Event.h
@@ -17,18 +17,18 @@ namespace DERPY
         MouseButtonPressed, MouseButtonReleasted, MouseMoved, MouseScrolled
     };
 
+    #define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+								virtual EventType GetEventType() const override { return GetStaticType(); }\
+								virtual const char* GetName() const override { return #type; }
+
 	class DERPY_API Event
     {
     public:
-		virtual EventType GetEventType() const = 0;
+		EventType GetEventType() const = 0;
 	    virtual const char* GetName() const = 0;
     	virtual int GetCategoryFlags() const = 0;
     	virtual std::string ToString() const { return GetName(); }
 
-    	inline bool IsInCategory(EventCategory category)
-    	{
-    		return GetCategoryFlags() & category;
-    	}
 	protected:
 	    bool m_Handled = false;
     };
