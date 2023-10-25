1e54e91aad9ab2a3bc66a6622554b09cbaa2ea3c
Badusername2262 <Badusername2262@gmail.com>
2023-10-26 07:34:00 +1000

    added CMakeLists.txt to root dir so can be compiled for visual studio

--- Engine/src/Derpy/Window.h
+++ Engine/src/Derpy/Window.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "../../external/GLFW/include/GLFW/glfw3.h"
 
 namespace DERPY
 {
@@ -13,9 +14,26 @@ namespace DERPY
         WindowProperties(const std::string& title = "Derpy Engine", unsigned int width = 1280, unsigned height = 720)
             :   Title(title), Width(width), Height(height)
         {
-
+            
         }
+    };
 
-        
+    class DERPY_API Window
+    {
+        public:
+            using EventCallback = std::function<void(Event&)>;
+
+            ~Window() {}
+
+            void OnUpdate();
+
+            unsigned int GetWidth() const;
+            unsigned int GetHeight() const;
+
+            void SetEventCallback(const EventCallback& callback);
+            void SetVSync(bool enabled);
+            bool IsVSync() const;
+
+            static Window* Create(const WindowProperties& Properties = WindowProperties());
     };
 }
